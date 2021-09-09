0481a727bbe69e0a1db363924caa935771ebaab8
Rolando Campusano <rcampusa@gmail.com>
2021-09-09 07:30:18 -0400

    some small changes to config

--- nmlogit_config.py
+++ nmlogit_config.py
@@ -1,3 +1,5 @@
+## THIS CODE IS THE CONFIG FILE FOR NMLOGIT
+
 #for distance function
 from math import sin, cos, sqrt, atan2, radians
 
@@ -6,8 +8,8 @@ import pandas as pd
 
 #for weighted sum function
 import numpy as np
-#define choice-alternative function
 
+#define choice-alternative functions
 def distance(data, train_config):
 
 	lon1 = data['lon'].values
@@ -21,18 +23,18 @@ def distance(data, train_config):
 	lon2 = np.where(abs(max_lon - lon1)>abs(min_lon-lon1),min_lon,max_lon)
 
 	lon1, lat1, lon2, lat2 = map(np.radians, [lon1, lat1, lon2, lat2])
-	
+
 	dlon = lon2 - lon1
 	dlat = lat2 - lat1
-	
+
 	a = np.sin(dlat/2.0)**2 + np.cos(lat1) * np.cos(lat2) * np.sin(dlon/2.0)**2
-	
+
 	c = 2 * np.arcsin(np.sqrt(a))
-	
+
 	km = 6367 * c
 
 	km = np.where((min_lon<=lon1) &
-				  (lon1<=max_lon) & 
+				  (lon1<=max_lon) &
 				  (min_lat<=lat1) &
 				  (max_lat>=lat1),0,km)
 
@@ -41,16 +43,17 @@ def distance(data, train_config):
 def athome(data,train_config):
 	athome = np.where((data['distance'].values==0) | (data['aux'].values==1), 1, 0)
 	return athome
-   
+
+#config dictionary
 config = {
 
 	'drop_vars':['lat','lon','max_lat','max_lon','min_lon','min_lat','aux'] ,
 	# options: BinaryCrossEntropy, MaxLogLikelihood
 	#'loss':  'MaxLogLikelihood',
 	'loss':  'MaxLogLikelihood',
-	
+
 	'expand': True,
-	
+
 	'optimizer': 'Adam',  # options:  Adam, RMSprop, SGD, LBFGS.
 	# Adam would converge much faster
 	# LBFGS is a very memory intensive optimizer (it requires additional param_bytes * (history_size + 1) bytes).
@@ -59,40 +62,40 @@ config = {
 	'learning_rate': 0.1, # Applicable to Adam, SGD, and LBFGS
 	# The learning_rate parameter seems essential to LBFGS, which converges in two epochs.
 	#  So far, learning_rate == 0.1 seems to be ok for LBFGS
-	
+
 	#'momentum': 0.9,  # applicable to SGD, RMSprop
 	'momentum': 0.01,  # applicable to SGD, RMSprop
-	
+
 	# The resulting model seems to be more balanced, i.e. no extreme large/small weights,
 	#  although one might not have the most ideal performance, i.e. high top_5_rank etc.
 	'weight_decay': 0, # Applicable to Adam, RMSprop and SGD
-	
+
 	# indicates the number of sessions included in each batch
 	'batch_size': 500,
-	
+
 	#maximum number of epochs
 	'epochs': 20,
-	
+
 	#tolerance for early stopping
 	'early_stop_min_delta': 1e-4,
 	'patience': 5.
-	
+
 	#if able to use GPU (unfortunately I am not able to do this)
 	'gpu': False,  # luckily, running on GPU is faster than CPU in this case.
-	
+
 	# level of logging, 0: no log,  1: print epoch related logs;  2: print session related logs
 	'verbose': 1,
-	
+
 	# Adding the regularization degredates the performance of model
 	#   which might suggests that the model is still underfitting, not overfitting.
 	'l1_loss_weight': 0,  # e.g. 0.001 the regularization that would marginalize the weights
 	'l2_loss_weight': 0,
-	
+
 	# flag indicates whether to save gradients during the training
-	'save_gradients': True, 
-	
+	'save_gradients': True,
+
 	#choice alternative function name
 	'session_alter_fn': ['distance','athome'],
 	'distance' : distance,
-	'athome' : athome	
+	'athome' : athome
 }
